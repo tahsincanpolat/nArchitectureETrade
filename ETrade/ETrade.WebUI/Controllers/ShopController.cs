b562539254106979b8d7ca5915556447ea474c2b
tahsincanpolat <tahsincanpolat@gmail.com>
2022-11-13 12:42:37 +0300

    shop

---
+++ ETrade/ETrade.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,57 @@
+﻿using ETrade.Business.Abstract;
+using ETrade.Entities;
+using ETrade.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETrade.WebUI.Controllers
+{
+    public class ShopController : Controller
+    {
+
+        private IProductService _productService;
+
+
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;   
+        }
+
+       [Route("products/{category?}")] // {category} => slug products/elektronik
+       public IActionResult List(string category,int page=1)
+       {
+            const int pageSize = 3;
+
+            var products = new ProductListModel()
+            {
+                PageInfo = new PageInfo()
+                {
+                    TotalItems = _productService.GetCountByCategory(category),
+                    ItemsPerPage = pageSize,
+                    CurrentCategory = category,
+                    CurrentPage = page
+                },
+                Products = _productService.GetProductsByCategory(category,page,pageSize)
+            };
+
+            return View(products);
+       } 
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = _productService.GetProductDetails(id.Value);
+
+            return View(new ProductDetailsModel()
+            {
+                Product = product,
+                Categories = product.ProductCategories.Select(i => i.Category).ToList()
+            });  
+        }
+
+
+    }
+}
---
+++ ETrade/ETrade.WebUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using ETrade.Business.Abstract;
+using ETrade.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETrade.WebUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService; 
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(new CategoryListViewModel()
+            {
+                SelectedCategory = RouteData.Values["category"]?.ToString(),
+                Categories = _categoryService.GetAll()
+            });
+        }
+    }
+}
